a49243863f3e11719f4f302a54800d252219e0dd
ddanny <dannz@126.com>
2022-04-28 13:12:18 +0800

    learning react (JSX) finished

--- src/App.js
+++ src/App.js
@@ -1,25 +1,94 @@
-import logo from './logo.svg';
+// import logo from './logo.svg';
 import './App.css';
+import ReactDOM from 'react-dom';
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+
+    let a = 1
+    for (let i = 1; i < 100; i++) {
+
+        console.log(i)
+
+
+    }
+    return (
+
+        <p>dfsdfsdffgggf fdgdfg fgfg fgfgfddd</p>
+
+
+    );
 }
+
+
+const data = [{
+    name: "Baked Salmon",
+    ingredients: [
+        {name: "Salmon", amount: 1, measurement: "l lb"},
+        {name: "Pine Nuts", amount: 1, measurement: "cup"},
+        {name: "Butter Lettuce", amount: 2, measurement: "cups"}, {
+            name: "Yellow Squash",
+            amount: 1,
+            measurement: "med"
+        },
+        {name: "Olive Oil", amount: 0.5, measurement: "cup"},
+        {name: "Garlic", amount: 3, measurement: "clovesd"}
+    ], steps: [
+        "Preheat the oven to 350 degrees.",
+        "Spread the olive oil around a glass baking dish.",
+        "Add the yellow squash and place in the oven for 30 mins.", "Add the salmon, garlic, and pine nuts to the dish.",
+        "Bake for 15 minutes.",
+        "Remove from oven. Add the lettuce and serve."
+    ]
+},
+    {
+        name: "Fish Tacos",
+        ingredients: [
+            {name: "Whitefish", amount: 1, measurement: "l lb"},
+            {name: "Cheese", amount: 1, measurement: "cup"},
+            {name: "Iceberg Lettuce", amount: 2, measurement: "cups"},
+            {name: "Tomatoes", amount: 2, measurement: "large"},
+            {name: "Tortillas", amount: 3, measurement: "med"}
+        ], steps: [
+            "Cook the fish on the grill until hot.", "Place the fish on the 3 tortillas.",
+            "Top them with lettuce, tomatoes, and cheese."
+        ]
+    }
+];
+
+function Recipe({name, ingredients, steps}) {
+    return (
+        <section id={name.toLowerCase().replace(/ /g, "-")}><h1>{name}</h1>
+            <ul className="ingredients">
+                {ingredients.map((ingredient, i) => (<li key={i}>{ingredient.name}</li>
+                ))}
+            </ul>
+            <section className="instructions">
+                <h2>Cooking Instructions</h2> {steps.map((step, i) => (
+                <p key={i}>{step}</p>
+            ))}
+            </section>
+        </section>
+    );
+}
+
+function Menu({title, recipes}) {
+    return (
+        <article>
+
+            <header>
+                <h1>{title}</h1>
+            </header>
+            <div className="recipes">
+                {recipes.map((recipe, i) => (<Recipe key={i} {...recipe} />
+                ))}
+            </div>
+        </article>
+    );
+}
+
+
+ReactDOM.render(
+<Menu recipes={data} title="Delicious Recipes" />, document.getElementById("recipes")
+);
 
 export default App;
