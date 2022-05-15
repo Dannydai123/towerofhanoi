f5d79e77430d585ce57966bf6df77e5e15295417
ddanny <ddanny@vmware.com>
2022-05-15 10:53:59 +0800

    Q1 welcome to react

--- src/App.js
+++ src/App.js
@@ -1,7 +1,10 @@
 // import logo from './logo.svg';
+
+// 45 practices at https://contactmentor.com/best-react-projects-for-beginners-easy/
+
 import './App.css';
 import ReactDOM from 'react-dom';
-
+import {Component} from "react";
 import 'bootstrap/dist/css/bootstrap.min.css';
 
 // import {$} from 'jquery'
@@ -10,8 +13,25 @@ import 'bootstrap/dist/js/bootstrap.bundle.min';
 import * as PropTypes from "prop-types";
 
 
+class App extends Component {
 
+//     constructor() {
+//         super();
+//     }
+// }
+//
+//     state = {
+//
+//
+//     };
 
-
+    render() {
+        return (
+            <div className='container'>
+                <p className='text-center h2'>Welcome to React</p>
+            </div>
+        );
+    }
+}
 
 export default App;
