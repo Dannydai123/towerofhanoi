0eb2b58c922af76f555b446bfbe35fe7a1dc77d1
Dannydai123 <61226544+Dannydai123@users.noreply.github.com>
2022-10-19 15:45:42 +0800

    Update index.js

--- src/index.js
+++ src/index.js
@@ -5,7 +5,7 @@ import App from "./Components/Tower_of_Hanoi/App";
 import reportWebVitals from "./reportWebVitals";
 
 import { Auth0Provider } from "@auth0/auth0-react";
-import App3 from "./App3";
+//import App3 from "./App3";
 // import TodosApp from "./App2";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
